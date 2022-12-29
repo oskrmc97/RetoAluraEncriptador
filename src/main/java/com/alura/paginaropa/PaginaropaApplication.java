0e8eca7d9f86a885670412b1d3367008b35e67d4
Oscar D. Melo <oskr975@gmail.com>
2022-12-29 14:45:00 -0500

    Reto Alura plantilla inicial de aqui se desprenden las ramas

---
+++ src/main/java/com/alura/paginaropa/PaginaropaApplication.java
@@ -0,0 +1,13 @@
+package com.alura.paginaropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaropaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaropaApplication.class, args);
+	}
+
+}

2461b09b286a01478d6e871065a0a205aeffc5b7
Oscar Melo <oskr975@gmail.com>
2023-04-20 19:36:02 -0500

    Proyecto encriptador

---
+++ src/main/java/com/alura/Encriptador/Encriptador-LAPTOP-DE7MV2M0.java
@@ -0,0 +1,13 @@
+package com.alura.Encriptador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Encriptador {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Encriptador.class, args);
+	}
+
+}
--- src/main/java/com/alura/Encriptador/controller/indexcontroller.java
+++ src/main/java/com/alura/Encriptador/controller/indexcontroller.java
@@ -1,4 +1,4 @@
-package com.alura.paginaropa.controller;
+package com.alura.Encriptador.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
